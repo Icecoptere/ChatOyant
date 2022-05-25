799ef867f0d50d6278b0723a30a35ca8ab1a5f08
Amaury ASTIER <48201453+Icecoptere@users.noreply.github.com>
2022-05-25 23:55:09 +0200

    Improve

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ console.log("On lance le Chat Oyant");
 
 const tmi = require('tmi.js');
 require('dotenv').config();
+const env = process.env.CURRENTENV;
+
+const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+
+function outsideFunction(args){
+    return `Success ${args[0]}`;
+}
+
+const commands = {
+    website: {
+        response: 'https://spacejelly.dev'
+    },
+    upvote: {
+        response: function(args){ outsideFunction(args)}
+    }
+}
 
 const client = new tmi.Client({
     connection: {
@@ -20,15 +36,32 @@ const client = new tmi.Client({
 client.connect();
 
 client.on('message', async (channel, context, message) => {
-    console.log('channel', {
-        channel,
-        user: context.username,
-        message,
-        context: context,
-    });
     const isNotBot = context.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME.toLowerCase();
 
     if (isNotBot) {
-        client.say(channel, `Responding to ${context.username} message: ${message}`);
+        let [raw, command,argument] = [undefined,undefined,undefined];
+        try{
+            [raw, command, argument] = message.match(regexpCommand);
+            console.log(raw, command, argument);
+        }catch (e){
+            console.log("there was an error :"+e);
+        }
+        if ( command ) {
+            console.log("Command detected");
+            const { response } = commands[command] || {};
+            if ( typeof response === 'function' ) {
+                client.say(channel, response(argument.split(" ")));
+            } else if ( typeof response === 'string' ) {
+                client.say(channel, response);
+            }
+        }else{
+            if(env === "DEV"){
+                console.log('channel', {
+                    channel,
+                    user: context.username,
+                    message,
+                });
+            }
+        }
     }
 });
